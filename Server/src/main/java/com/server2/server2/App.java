d5b1463934d22f35b259fa1ab3263c6e438e6f1e
Kwangwook-Kwon <kwangwook.kwon@gmail.com>
2019-04-22 08:24:06 +0000

    basic server

---
+++ Server/src/main/java/com/server2/server2/App.java
@@ -0,0 +1,16 @@
+package com.server2.server2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class App {
+
+	public static void main(String[] args) {
+    	System.out.println("Hello World");
+    	SpringApplication.run(App.class, args);
+	}
+
+}
